e5232a6afbd90d3c36440ffcb3761a6072df323f
Mephistopheres <xiong.daniel@gmail.com>
2017-06-07 19:17:19 +0800

    Added resources
    Could be opened with VS

---
+++ Lifesaver/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If PictureBox1.Left <= PictureBox2.Left Or PictureBox1.Top <= PictureBox2.ToThen Then
+
+        End If
+    End Sub
